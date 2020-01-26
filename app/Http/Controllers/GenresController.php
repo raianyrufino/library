e0b061f6f44815b0cdd98b0cd8baa752e9816093
Raiany <raianyrufino12@gmail.com>
2020-01-26 13:11:23 -0300

    create function index

--- app/Http/Controllers/GenresController.php
+++ app/Http/Controllers/GenresController.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\{Author, Book, Genre};
 use Illuminate\Http\Request;
 
 class GenresController extends Controller
 {
-    //
+    public function index(){
+        $genres = Genre::all()->sortBy('name');
+        //return view('genres.index', compact('genres'));
+    }
 }
